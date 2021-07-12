c6470acc35bbc2ccae12179fc8f8e4977a68ccdf
shi-n <shi-n_xyz@cloudsquare.jp>
2021-07-12 21:22:26 +0900

    アルバム画面（再生制御）再表示ボタン追加

--- ExampleMusicKit/AlbumShuffle.swift
+++ ExampleMusicKit/AlbumShuffle.swift
@@ -10,21 +10,33 @@ import SwiftUI
 struct AlbumShuffle: View {
     @EnvironmentObject var music: Music
     @State var searchText: String = ""
+    @State var selection: Int = 0
     
     var body: some View {
         NavigationView {
-            List {                
-                ForEach(self.music.artists) { artist in
-                    NavigationLink(artist.name, destination: {
-                        AlbumView(artist: artist)
-                            .environmentObject(self.music)
-                    })
+            List {
+                Section("Search") {
+//                    Picker("Type", selection: self.$selection) {
+//                        Text("Artist")
+//                        Text("Album")
+//                    }
+//                    .pickerStyle(.segmented)
+                    TextField("Search Text", text: self.$searchText)
+                }
+                Section("Result") {
+                    ForEach(self.music.artists) { artist in
+                        NavigationLink(artist.name, destination: {
+                            AlbumView(artist: artist)
+                                .environmentObject(self.music)
+                        })
+                    }
+
                 }
             }
-            .navigationTitle("Artist Search")
+            .navigationTitle("Search")
         }
-        .searchable(text: self.$searchText, placement: .automatic, prompt: "Artist")
-        .onSubmit(of: .search) {
+//        .searchable(text: self.$searchText, placement: .automatic, prompt: "Artist")
+        .onSubmit(of: .text) {
             print(self.searchText)
             self.music.searchArtists(searchText: self.searchText)
         }
